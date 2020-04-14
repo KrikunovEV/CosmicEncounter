98192271a7cab5377a139d7bc718790b988a4de2
Evgenii Krikunov <Jen9wka@gmail.com>
2020-04-14 20:42:22 +0300

    correct file

--- plot_graphics.py
+++ plot_graphics.py
@@ -21,8 +21,9 @@ for i in range(5):
     ax[1][i].plot(median_filter(values, 3))
     ax[1][i].set_xlabel('episode')
     ax[1][i].set_ylabel('mean value')
-    ax[1][i].set_yticks(np.arange(0, 20, 2))
+    ax[1][i].set_yticks(np.arange(0, 20, 2))  # 0, 20, 2 | 0, 30, 3
 fig.tight_layout()
 
-plt.show()
+plt.savefig('1000_episodes.png')
+#plt.show()
 
