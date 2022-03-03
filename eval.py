c8712172ec12c9eaa62bea5107fdc274d34d51b1
Evgenii Krikunov <Jen9wka@gmail.com>
2022-03-03 17:12:38 +0300

    keep safe 2

--- eval.py
+++ eval.py
@@ -13,20 +13,27 @@ negotiation_map_two = [[1], [0], [], [], []]
 negotiation_map_three = [[1, 2], [0, 2], [0, 1], [], []]
 negotiation_map_case1 = [[], [], [], [], []]
 negotiation_map_case2 = [[1, 2], [0, 2], [0, 1], [], []]
-agents = [Agent(0, players, negotiation_map_case1),
-          Agent(1, players, negotiation_map_case1),
-          Agent(2, players, negotiation_map_case1),
-          Agent(3, players, negotiation_map_case1),
-          Agent(4, players, negotiation_map_case1)]
-agents[0].load_agent_state('everyone/3.pt')
-agents[1].load_agent_state('nobody/4.pt')
-agents[2].load_agent_state('nobody/4.pt')
-agents[3].load_agent_state('nobody/4.pt')
+negotiation = [0, 1, 2, 3, 4]
+agents = [Agent(0, players, negotiation),
+          Agent(1, players, negotiation),
+          Agent(2, players, negotiation),
+          Agent(3, players, negotiation),
+          Agent(4, players, negotiation)]
+agents[0].load_agent_state('pool/20.pt')
+agents[1].load_agent_state('everyone/2.pt')
+agents[2].load_agent_state('pool/18.pt')
+agents[3].load_agent_state('pool/18.pt')
+agents[3].load_agent_state('pool/18.pt')
 agents[0].parties_won = 0
 agents[1].parties_won = 0
 agents[2].parties_won = 0
 agents[3].parties_won = 0
 agents[4].parties_won = 0
+agents[0].reward_cum = [0]
+agents[1].reward_cum = [0]
+agents[2].reward_cum = [0]
+agents[3].reward_cum = [0]
+agents[4].reward_cum = [0]
 
 for episode in range(1000):
     print('Episode:', episode)
@@ -42,26 +49,34 @@ for episode in range(1000):
             obs, terminal, winners, reward = env.action(env.available_actions()[np.random.randint(
                 len(env.available_actions()))], zeros(10, requires_grad=False))
         agents[agent_id].reward(reward)
+
+        new_negotiations = []
+        for i in range(players):
+            if i == agent_id or i == 4:
+                continue
+            negotiation = agents[i].get_negotiation(obs)
+            new_negotiations.append([i, negotiation])
+        env.update_negotiation(negotiations=new_negotiations)
+
         if terminal:
-            for agent_id in winners:
-                agents[agent_id].reward_win(reward)
+            for agent_id in range(players):
+                if agent_id in winners:
+                    agents[agent_id].reward_win(reward)
+                else:
+                    agents[agent_id].reward_loose()
 
 fig, ax = plt.subplots(1, 5, figsize=(16, 9))
 for agent_id in range(players):
-    print('Agent id: ' + str(agent_id) + '; wins: ' + str(agents[agent_id].parties_won) + '; score = ' + str(
-        agents[agent_id].reward_cum[-1]))
+    print('Agent id: ' + str(agent_id) + '; wins: ' + str(agents[agent_id].parties_won) + '; score = ' + '%.2f' % agents[agent_id].reward_cum[-1])
     if agent_id == 0:
-        ax[agent_id].set_title('Communication agent. wins:' + str(agents[agent_id].parties_won) + '; score = ' + str(
-        agents[agent_id].reward_cum[-1]))
+        ax[agent_id].set_title('Newbie agent. wins:' + str(agents[agent_id].parties_won) + '; score = ' + '%.2f' % agents[agent_id].reward_cum[-1])
     elif agent_id != 4:
-        ax[agent_id].set_title('Noncommunication agent. wins:' + str(agents[agent_id].parties_won) + '; score = ' + str(
-        agents[agent_id].reward_cum[-1]))
+        ax[agent_id].set_title('Pool agent. wins:' + str(agents[agent_id].parties_won) + '; score = ' + '%.2f' % agents[agent_id].reward_cum[-1])
     else:
-        ax[agent_id].set_title('Random agent. wins:' + str(agents[agent_id].parties_won) + '; score = ' + str(
-        agents[agent_id].reward_cum[-1]))
+        ax[agent_id].set_title('Random agent. wins:' + str(agents[agent_id].parties_won) + '; score = ' + '%.2f' % agents[agent_id].reward_cum[-1])
     ax[agent_id].plot(agents[agent_id].reward_cum)
     ax[agent_id].set_xlabel('episode')
     ax[agent_id].set_ylabel('cumulative reward')
     ax[agent_id].set_yticks(np.arange(0, 801, 100))
 fig.tight_layout()
-plt.savefig('eval/exp_1.png')
+plt.savefig('eval/exp_2.png')
